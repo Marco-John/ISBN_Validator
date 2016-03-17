10fbc66f098a1988971afa8e26c6db90fc013420
Marco-John <john.throckmorton4@gmail.com>
2016-03-16 23:55:40 -0400

    Enter Strings

--- ISBN_Validator.rb
+++ ISBN_Validator.rb
@@ -1,1 +1,9 @@
-# ISBN Validator
+def valid_isbn?(input_string)
+	if input_string.length == 10 
+		true
+	elsif input_string.length == 13
+		true
+	else
+		false
+	end	
+end
--- Test_ISBN_Validator.rb
+++ Test_ISBN_Validator.rb
@@ -2,11 +2,23 @@
   
 #Create a program that will determine if a string is a valid ISBN-10 or ISBN-13 number is valid.
 
+require 'minitest/autorun'
+require_relative 'ISBN_Validator.rb'
+
+class TestIsbnChecker < Minitest::Test
+	  
 #1.  ISBN-10 is made up of 9 digits and and a check digit (which may be "x")
 #2.  ISBN-13 is made up of 12 digits and a check digit
 #3.  Remove spaces and hyphens from string
 
-
+	def test_string_without_10_or_13_numbers
+		assert_equal(false, valid_isbn?(""))
+		assert_equal(false, valid_isbn?("123456789123"))
+		assert_equal(false, valid_isbn?("12345678912345"))
+	end
+	
+	
+end
 
 #ISBN-10
 
